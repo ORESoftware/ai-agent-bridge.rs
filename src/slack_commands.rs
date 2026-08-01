96ac362f547c28451bad320f71a65cd9033dcf65
Alexander Mills <alex@oresoftware.com>
2026-08-01 15:42:01 -0500

    DEN-1041: include installed Slack app contract

--- src/slack_commands.rs
+++ src/slack_commands.rs
@@ -11,3 +11,4 @@ include!("slack_commands_parts/part5.rs");
 include!("slack_commands_parts/part6.rs");
 include!("slack_commands_parts/part7.rs");
 include!("slack_commands_parts/part8.rs");
+include!("slack_commands_parts/part9.rs");
