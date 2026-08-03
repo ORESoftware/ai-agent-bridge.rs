9f61ab83d66782f8166ea5fe7944e228dcdc5441
Alexander Mills <alex@oresoftware.com>
2026-08-03 11:21:34 -0500

    feat(DEN-1061): include observable event projection module

--- src/slack_commands.rs
+++ src/slack_commands.rs
@@ -13,3 +13,4 @@ include!("slack_commands_parts/part7.rs");
 include!("slack_commands_parts/part8.rs");
 include!("slack_commands_parts/part9.rs");
 include!("slack_commands_parts/part10.rs");
+include!("slack_commands_parts/part11.rs");
